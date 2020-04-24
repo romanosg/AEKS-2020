c23593c63b9c16d0aa792d774a1988bf1c515e91
romanosg <romanosg@csd.auth.gr>
2020-04-24 21:32:44 +0300

    added already existed class gametype1 xml and its activity class

---
+++ MemoryGame/app/src/main/java/com/example/MemoryGameActivities/game1Activity.java
@@ -0,0 +1,14 @@
+package com.example.MemoryGameActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class game1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game1);
+    }
+}
