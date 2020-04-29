4fad25c063e7cd19900829af42a15078b67dc8c3
rrgeorgio <rrgeorgio@csd.auth.gr>
2020-04-29 11:03:55 +0300

    Connected the main menu activities with the main menu

--- MemoryGame/app/src/main/java/com/android/MemoryGameActivities/MainActivity.java
+++ MemoryGame/app/src/main/java/com/android/MemoryGameActivities/MainActivity.java
@@ -2,7 +2,9 @@ package com.android.MemoryGameActivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.memory_game.app.R;
 
@@ -12,6 +14,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startGame(View view){
+        Intent i = new Intent(this,ModeSelection.class);
+        startActivity(i);
+    }
+
+    public void rulesActivity(View view){
+        Intent i= new Intent(this,Rules.class);
+        startActivity(i);
+    }
+    public void highScoresActivity(View view){
+        Intent i = new Intent(this,Highscores.class);
+        startActivity(i);
+    }
+    public void findPlayerActivity(View view){
+        Intent i = new Intent(this,findPlayerActivity.class);
+        startActivity(i);
+
+    }
+
+    public void exitApplication(View view){
+        Intent i= new Intent(Intent.ACTION_MAIN);
+        i.addCategory(Intent.CATEGORY_HOME);
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(i);
     }
 
 
