cdf42c38e2e50a6cab68b3f2829d28d5e7d5428f
Ioanna Stainou <ioanna_stainou@yahoo.gr>
2020-05-16 18:54:03 +0300

    Easy bot mod 4 & 8.

---
+++ MemoryGame/app/src/main/java/com/memory_game/app/Bots/EasyBotMod8.java
@@ -0,0 +1,21 @@
+package com.memory_game.app.Bots;
+
+import java.util.Random;
+import com.memory_game.app.GameManagers.*;
+import com.memory_game.app.AndroidGui;
+
+
+public class EasyBotMod8 extends EasyBot{	
+	
+	public void OpenCard(AndroidGui gui){
+		while(true){
+			x = r.nextInt(GameManagerNormal.numberOfCards)+GameManagerNormal.numberOfCards;
+			if(x!=opened && x1[x-GameManagerNormal.numberOfCards]==0)break;
+		}
+		System.out.println("Played"+x);
+                gui.clicableButtons(x);
+		gui.clickButton(x);
+    	opened = x;
+    }
+
+}
